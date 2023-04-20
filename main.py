37e1009aec8408b75f61c0a4b7b318e0d642cfe2
ev-25889 <106339646+ev-25889@users.noreply.github.com>
2023-04-20 15:57:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,23 @@
-import telebot
-import time
-from telebot import types
-from pillow import create_picture
+import config
+import logging
 
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.message import ContentType
 
-# Создаем экземпляр бота
+# log
+logging.basicConfig(level=logging.INFO)
 
-bot = telebot.TeleBot('5978396970:AAEDoWW-BIFwYfyQ4bFJkuIKyeWjfH8OdlU')
+# init
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
 
-# Функция, обрабатывающая команду /start
+# echo bot
 
-@bot.message_handler(commands=["start"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Введите дату рождения в формате 01.01.2000')
-# Получение сообщений от юзера
+@dp.message_handler()
+async def echo(message; types.Message):
+    await message.answer (message.text)
 
+# run long-polling
 
-
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-
-    image_name = create_picture(message.text, message.chat.id)
-    #mess = 'число {}, месяц {}, год {}.'.format(hoy, mes, ano)
-    # bot.send_message(message.chat.id, image_name)
-    bot.send_photo(message.chat.id, photo=open(image_name, 'rb'))
-    time.sleep(2)
-    bot.send_message(message.chat.id, 'Выберете, описание какой энергии хотите получить')
-
-# Запускаем бота
-bot.polling(none_stop=True, interval=0)
-
-
-"""
-def send_photo_file(chat_id, img):
-    files = {'photo': open(img, 'rb')}
-    bot.send_message(chat.id, 'image.new_img.png')
-    requests.post(f'{URL}{TOKEN}/sendPhoto?chat_id={chat_id}', files=files)
-    """
+if __name__ == "main":
+    executor.start_polling(dp, skip_updates=False) # чтобы обрабатывалось каждое сообщение с сервера телеграм
